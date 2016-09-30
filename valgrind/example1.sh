4786064b1ea607621418cb09fe118205721f7a29
ThongLT <letrthong@gamil.com>
2016-10-01 01:01:21 +0700

    check memory leak

--- valgrind/example1.sh
+++ valgrind/example1.sh
@@ -7,3 +7,7 @@ valgrind --tool=memcheck ./example1
 echo  ""
 echo "Detect memory leak"
 valgrind --tool=memcheck --leak-check=yes ./example1
+
+echo  ""
+echo "Report"
+valgrind --tool=memcheck --leak-check=yes --xml=yes --xml-file=test.xml ./example1
