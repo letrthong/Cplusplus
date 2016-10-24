c6008fdd402a1884696593d8741dec6e4451fe24
ThongLT <letrthong@gamil.com>
2016-10-24 23:30:13 +0700

    queue

--- InterprocessCommunication/message_queue/send.cpp
+++ InterprocessCommunication/message_queue/send.cpp
@@ -1,6 +1,13 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
 #include <sys/msg.h>
 
 int main()
 {
+
+  key_t key; /* key to be passed to msgget() */ 
+  int msgflg;/* msgflg to be passed to msgget() */ 
+  int ret  = msgget(key, msgflg);
+
   return 0;
 }
