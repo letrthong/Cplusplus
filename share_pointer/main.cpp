6ee34380e755ba9c9dcc4354d97a9e18fcebdd74
Thong LT <letrthong@gmail.com>
2022-06-19 18:29:14 +0200

    share pointer

---
+++ share_pointer/main.cpp
@@ -0,0 +1,26 @@
+// std_memory_shared_from_this.cpp
+// compile with: /EHsc
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+struct base : public std::enable_shared_from_this<base>
+{
+    int val;
+    shared_ptr<base> share_more()
+    {
+        return shared_from_this();
+    }
+};
+
+int main()
+{
+    auto sp1 = make_shared<base>();
+    auto sp2 = sp1->share_more();
+
+    sp1->val = 3;
+    cout << "sp2->val == " << sp2->val << endl;
+    return 0;
+}
+//  https://docs.microsoft.com/vi-vn/cpp/standard-library/enable-shared-from-this-class?view=msvc-160
