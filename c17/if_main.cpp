e2131a4dd50152a4065d75bf48a4ccbbe3e1549b
Thong LT <letrthong@gmail.com>
2024-11-15 12:21:29 +0700

    Create if_main.cpp

---
+++ c17/if_main.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+* C++ 17
+* https://www.onlinegdb.com/online_c++_compiler#
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    
+     if( int i = 10; i < 5)
+     {
+          std::cout<<"Hello World";
+     }
+     else 
+     {
+          std::cout<<"Hello World  i=" << i;
+     }
+    return 0;
+}
